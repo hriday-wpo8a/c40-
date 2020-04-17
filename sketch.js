b6239c9f4d2ea5cb8966f1657a5ab49baeb04b72
hriday-wpo8a <58740006+hriday-wpo8a@users.noreply.github.com>
2020-04-17 14:40:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var gameState = 0;
+var distance  =0 ;
+var playerCount,database;
+var form,player,game;
+var allPlayers;
+var car1,car2,car3,car4,cars;
+var track,car1_image,car2_image,car3_image,car4_image,ground;
+function preload(){
+
+    track = loadImage("images/track.jpg");
+    car1_image = loadImage("images/car1.png");
+    car2_image = loadImage("images/car2.png");
+    car3_image = loadImage("images/car3.png");
+    car4_image = loadImage("images/car4.png");
+    ground = loadImage("images/ground.png");
+}
+
+    function setup(){
+    createCanvas(displayWidth-20,displayHeight-30);
+    database = firebase.database()
+    console.log(database);
+ game = new Game();
+ game.getState();
+ game.start();
+
+    
+   
+
+}
+
+function draw(){
+   background("white");
+   if(playerCount===4){
+    game.update(1);
+   }
+   if(gameState===1){
+       clear();
+       game.play();
+   }
+   if(gameState===2){
+       game.end();
+
+   }
+}
+
